43fb605dd4c506ebc91a7d7100bc97f64398dc75
archibob <36523826+archibob@users.noreply.github.com>
2025-04-14 15:50:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,14 +10,15 @@ API_URL = "https://api.skinport.com/v1/items?app_id=730&currency=EUR"
 KEYWORDS = ["Коготь", "Сажа"]  # Добавь свои ключи
 
 def send_telegram_message(message):
-    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
-    payload = {"chat_id": CHAT_ID, "text": message}
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"  # Здесь исправлено
+    payload = {"chat_id": TELEGRAM_CHAT_ID, "text": message}
     try:
         response = requests.post(url, data=payload)
         if response.status_code != 200:
             print("Ошибка отправки в Telegram:", response.text)
     except Exception as e:
         print("Ошибка Telegram:", e)
+
 
 def check_items():
     try:
