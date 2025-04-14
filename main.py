2a596b075709e3358afec9bac3ffcc1192695ca9
archibob <36523826+archibob@users.noreply.github.com>
2025-04-15 01:21:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ API_URL = "https://api.skinport.com/v1/items?app_id=730&currency=EUR"
 # 🧲 Ключевые слова и максимальные цены (в евро)
 ITEMS_PRICE_LIMITS = {
     "Talon Knife": 300,
-    "Sport Gloves | Bronze Morph": 150
+    "Skeleton Knife": 220
 }
 
 def send_telegram_message(message):
@@ -60,16 +60,16 @@ def check_items():
             if "talon knife" in market_name.lower():
                 print(f"Найдено соответствие для ножа: {market_name}")
                 if price is not None and price <= ITEMS_PRICE_LIMITS["Talon Knife"]:
-                    message = f"🔔 Найден нож:\n{market_name}\n💶 Цена: {price} EUR\n🔗 {item_url}"
+                    message = f"🔔 Найден Talon Knife:\n{market_name}\n💶 Цена: {price} EUR\n🔗 {item_url}"
                     print(message)
                     send_telegram_message(message)
                     matches_found += 1
 
-            # Проверка на Спортивные перчатки | Окисление бронзы
-            if re.search(r"Sport\s*Gloves\s*\|\s*Bronze\s*Morph", market_name, re.IGNORECASE):
-                print(f"Найдено соответствие для перчаток: {market_name}")
-                if price is not None and price <= ITEMS_PRICE_LIMITS["Sport Gloves | Bronze Morph"]:
-                    message = f"🔔 Найдены перчатки:\n{market_name}\n💶 Цена: {price} EUR\n🔗 {item_url}"
+            # Проверка на Skeleton Knife
+            if "skeleton knife" in market_name.lower():
+                print(f"Найдено соответствие для Skeleton Knife: {market_name}")
+                if price is not None and price <= ITEMS_PRICE_LIMITS["Skeleton Knife"]:
+                    message = f"🔔 Найден Skeleton Knife:\n{market_name}\n💶 Цена: {price} EUR\n🔗 {item_url}"
                     print(message)
                     send_telegram_message(message)
                     matches_found += 1
